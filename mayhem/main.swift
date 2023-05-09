eb07b69de5b8296ed1137362026fbc6f26606c4d
bcapuano <bcapuano@asu.edu>
2023-05-08 21:46:47 -0400

    New attemp with more coverage

--- mayhem/main.swift
+++ mayhem/main.swift
@@ -9,11 +9,31 @@ import MSVCRT
 import Foundation
 import Ink
 
-let parser = MarkdownParser()
+let possible_modifiers = [
+    Modifier.Target.metadataKeys,
+    Modifier.Target.metadataValues,
+    Modifier.Target.blockquotes,
+    Modifier.Target.codeBlocks,
+    Modifier.Target.headings,
+    Modifier.Target.horizontalLines,
+    Modifier.Target.html,
+    Modifier.Target.images,
+    Modifier.Target.inlineCode,
+    Modifier.Target.links,
+    Modifier.Target.lists,
+    Modifier.Target.paragraphs,
+    Modifier.Target.tables,
+]
 
 @_cdecl("LLVMFuzzerTestOneInput")
 public func test(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
     let fdp = FuzzedDataProvider(start, count)
+    let modifier = Modifier(target: fdp.PickValueInList(from: possible_modifiers))
+    { html, markdown in
+        return fdp.ConsumeRemainingString() + html
+    }
+
+    let parser = MarkdownParser(modifiers: [modifier])
     parser.html(from: fdp.ConsumeRemainingString())
     return 0;
 }
